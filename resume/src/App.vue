d851d0f79e7e34f96ecddd2f8068df431c746e96
mdiasanta <markdiasanta@gmail.com>
2021-02-22 01:30:11 -0500

    Added navigation, basic profile, lint rule

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,47 +1,86 @@
 <template>
-  <v-app>
+  <v-app style="background-color:#505251;">
     <v-app-bar app color="#3F51B5" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          :src="require('@/assets/img/bape.png')"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <span class="headline">
-          Mark Diasanta
-        </span>
-      </div>
+      <v-app-bar-title>
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            :src="require('@/assets/img/bape.png')"
+            transition="scale-transition"
+            width="40"
+          />
+          <span class="headline">Mark Diasanta</span>
+        </div>
+      </v-app-bar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Switch Languages</span>
-        <v-icon>fa-language</v-icon>
-      </v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fa-home</v-icon>
+          </v-btn>
+        </template>
+        <span>Home</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fa-user-circle</v-icon>
+          </v-btn>
+        </template>
+        <span>About Me</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fa-briefcase</v-icon>
+          </v-btn>
+        </template>
+        <span>Resume</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fab fa-linkedin</v-icon>
+          </v-btn>
+        </template>
+        <span>LinkedIn</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fab fa-github</v-icon>
+          </v-btn>
+        </template>
+        <span>GitHub</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon>fa-language</v-icon>
+          </v-btn>
+        </template>
+        <span class>Switch Languages</span>
+      </v-tooltip>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <Profile />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Profile from '@/components/Profile';
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
-    HelloWorld
+    Profile
   },
 
   data: () => ({
---
+++ resume/src/components/Profile.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card
+    flat
+    dark
+    style="width:500px;background-color:transparent; transform:translate(100%)"
+  >
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col align-self="center">
+        <v-img
+          contain
+          :src="require('@/assets/img/photo.jpg')"
+          transition="scale-transition"
+          width="50%"
+          style="border-radius:50%; transform:translate(50%)"
+        />
+      </v-col>
+      <v-col>
+        <v-list-item>
+          <v-list-item-content class="text-center">
+            <v-list-item-title class="title">
+              Mark Diasanta
+            </v-list-item-title>
+            <v-list-item-subtitle>Software Engineer</v-list-item-subtitle>
+            <v-list-item-subtitle>Virginia Beach</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({})
+};
+</script>
