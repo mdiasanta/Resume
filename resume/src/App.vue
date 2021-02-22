b241647260bfd42e731b75d5f645f35cca5b7bdd
mdiasanta <markdiasanta@gmail.com>
2021-02-21 21:01:15 -0500

    started replacing menu

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="#3F51B5" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('@/assets/img/bape.png')"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="headline">
+          Mark Diasanta
+        </span>
       </div>
 
       <v-spacer></v-spacer>
@@ -28,8 +23,8 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Switch Languages</span>
+        <v-icon>fa-language</v-icon>
       </v-btn>
     </v-app-bar>
 
